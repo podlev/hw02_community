5a52c007dd30dac8a867957011c208ea7210063a
Лев Подъельников <podlev@gmail.com>
2022-04-06 16:52:36 +0300

    Проект готов

---
+++ yatube/posts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'Posts'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('group/<slug:slug>/', views.group_post, name='group_list'),
+]
---
+++ yatube/posts/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
+
+
+def index(request):
+    template = 'posts/index.html'
+    title = 'Это главная страница проекта Yatube'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {'title': title,
+               'posts': posts}
+    return render(request, template, context)
+
+
+def group_post(request, slug):
+    template = 'posts/group_list.html'
+    title = 'Записи сообщества '
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {'title': title,
+               'posts': posts,
+               'group': group}
+    return render(request, template, context)
